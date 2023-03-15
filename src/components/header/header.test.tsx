5c4ce74349a591df654ee7e6c156effca8cdc9f1
Covadonga M <cova.millan@gmail.com>
2023-03-15 13:52:39 +0100

    Add header test

---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { Header } from "./header";
+describe("Given the header component", () => {
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    render(<Header />);
+  });
+  describe("When its rendered", () => {
+    test("Then it should contain the word", () => {
+      const img = screen.getByAltText(/logo/i);
+      expect(img).toBeInTheDocument();
+    });
+    test("Then it should contain role", () => {
+      const element = screen.getByRole("heading");
+      expect(element).toBeInTheDocument();
+    });
+
+    test("Then it should contain img role", () => {
+      const element = screen.getByRole("img");
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
