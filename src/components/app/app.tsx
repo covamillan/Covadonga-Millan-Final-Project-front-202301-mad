a7173ba5187dc98401e916227b50266b324ab135
Covadonga M <cova.millan@gmail.com>
2023-03-16 13:34:02 +0100

    Update some files

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../header/header";
+import Register from "../register/register";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Register></Register>
     </div>
   );
 }
