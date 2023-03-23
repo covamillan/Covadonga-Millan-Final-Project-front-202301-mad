385288057522e0358d03f074f5af233f20fa8d4f
Covadonga M <cova.millan@gmail.com>
2023-03-23 10:09:07 +0100

    Add firebase config

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCYKgyNdbxnxpRPzfcTFDslKI2Dltq3hAg",
+  authDomain: "pet-hospital-1e2b6.firebaseapp.com",
+  projectId: "pet-hospital-1e2b6",
+  storageBucket: "pet-hospital-1e2b6.appspot.com",
+  messagingSenderId: "264765845209",
+  appId: "1:264765845209:web:6b33f1b0e3b38adb95d8c8",
+};
