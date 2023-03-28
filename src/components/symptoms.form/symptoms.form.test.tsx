1ab95ae229b63ecd69c55912055a624f6bfabf6d
Covadonga M <cova.millan@gmail.com>
2023-03-28 22:27:24 +0200

    Add symptoms form test

--- src/components/symptoms.form/symptoms.form.test.tsx
+++ src/components/symptoms.form/symptoms.form.test.tsx
@@ -1,37 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/no-render-in-setup */
 import { act, fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
 import { usePets } from "../../hooks/usePets";
 import { PetsRepo } from "../../services/pets/pet.repo";
 import { store } from "../../store/store";
+import AddSymptoms from "./symptoms.form";
 
-import AddPet from "./add.pet";
 const mockParams = { id: "1" };
 jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
+  ...(jest.requireActual("react-router-dom") as any),
   useParams: () => mockParams,
+  useNavigate: () => jest.fn().mockImplementation(() => ({})),
 }));
 jest.mock("../../hooks/usePets.ts");
 jest.mock("../../firebase/firebase.pet.ts");
 
-describe("Given the add pet component", () => {
+describe("Given the add symptoms component", () => {
   beforeEach(async () => {
     (usePets as jest.Mock).mockReturnValue({
       petsState: {
-        pets: [
+        pet: [
           {
             id: "1",
-            name: "firulais",
-          },
-          {
-            id: "1",
-            name: "a",
+            meds: "abrazos",
           },
         ],
+        actualPet: { id: "1", meds: "besitos" },
       },
-      createNewPet: jest.fn(),
       updatePetId: jest.fn(),
     });
 
@@ -39,7 +38,7 @@ describe("Given the add pet component", () => {
       render(
         <Provider store={store}>
           <MemoryRouter>
-            <AddPet />
+            <AddSymptoms />
           </MemoryRouter>
         </Provider>
       );
@@ -47,7 +46,6 @@ describe("Given the add pet component", () => {
   });
   describe("When we render the component and do an update", () => {
     const mockRepo = {
-      createPetRepo: jest.fn(),
       updatePetRepo: jest.fn(),
     } as unknown as PetsRepo;
     test("Then it should contain the 'button' role", async () => {
@@ -71,46 +69,6 @@ describe("Given the add pet component", () => {
     test("Then it should contain the 'textbox' role", () => {
       const elements = [screen.getAllByRole("textbox")];
       expect(elements.length).toBe(1);
-    });
-  });
-});
-
-describe("When we want to create a pet", () => {
-  beforeEach(async () => {
-    (usePets as jest.Mock).mockReturnValue({
-      petsState: {
-        pets: [{ id: "4" }],
-      },
-      createNewPet: jest.fn(),
-      updatePetId: jest.fn(),
-    });
-
-    await act(async () => {
-      render(
-        <Provider store={store}>
-          <MemoryRouter>
-            <AddPet />
-          </MemoryRouter>
-        </Provider>
-      );
-    });
-  });
-  const mockRepo = {
-    createPetRepo: jest.fn(),
-    updatePetRepo: jest.fn(),
-  } as unknown as PetsRepo;
-  describe("When we render the component", () => {
-    test("Then it should contain the 'button' role", async () => {
-      const elements = [screen.getByRole("button")];
-      await fireEvent.click(elements[0]);
-    });
-
-    test("Then it should call the create function", async () => {
-      await act(async () => {
-        fireEvent.click(screen.getByRole("button"));
-      });
-      const { createNewPet } = usePets(mockRepo);
-      expect(createNewPet).toHaveBeenCalledTimes(1);
     });
   });
 });
